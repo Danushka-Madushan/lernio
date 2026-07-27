7d1e68c539b4a3a807d1d754cd5bc2ddfe9ffca8
Danushka-Madushan <danushkamadushan128@gmail.com>
2026-07-28 02:05:17 +0530

    update StudentMeetingsNav.tsx

--- src/components/StudentMeetingsNav.tsx
+++ src/components/StudentMeetingsNav.tsx
@@ -18,6 +18,7 @@ function formatDate(dateStr: string) {
   return new Date(dateStr).toLocaleString('en-GB', {
     day: '2-digit', month: 'short', year: 'numeric',
     hour: '2-digit', minute: '2-digit',
+    timeZone: 'Asia/Colombo',
   });
 }
 
